16562f1d09c50dc0213dc7534c952da3dec32a01
Adam-Arezza <arezza.adam@gmail.com>
2019-02-11 16:20:07 -0500

    first commit, working application

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,127 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Sticky Board</h2>
+    <div id="stickyBoard">
+      <div class="container-fluid">
+        <input id="noteInput" v-model="task" type="text" placeholder="write note">
+        <input v-model="owner" type="text" placeholder="assign to">
+        <b-button id="addBtn" size="md" @click="addSticky">Add Note</b-button>
+      </div>
+      <div id="board" class="row no-gutters">
+        <div id="newNotes" class="col-sm-4">
+          <h2>Tasks</h2>
+          <sticky-note
+            v-for="(note, index) in this.newNotes"
+            :key="index"
+            :task="note.task"
+            :owner="note.owner"
+            @deleteNote="removeSticky(index,note)"
+            @moveNote="moveSticky(index,note)"
+            @assign="assignOwner"
+          ></sticky-note>
+        </div>
+        <div id="inProgress" class="col-sm-4">
+          <h2>In Progress</h2>
+          <sticky-note
+            v-for="(note, index) in this.inProgress"
+            :key="index"
+            :task="note.task"
+            :owner="note.owner"
+            @deleteNote="removeSticky(index,note)"
+            @moveNote="moveSticky(index,note)"
+            @assign="assignOwner"
+          ></sticky-note>
+        </div>
+        <div id="completed" class="col-sm-4">
+          <h2>Completed</h2>
+          <sticky-note
+            v-for="(note, index) in this.complete"
+            :key="index"
+            :task="note.task"
+            :owner="note.owner"
+            @deleteNote="removeSticky(index,note)"
+            @moveNote="moveSticky(index,note)"
+            @assign="assignOwner"
+          ></sticky-note>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import stickyNote from "./components/sticky-note.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components: { stickyNote },
+  data() {
+    return {
+      task: "",
+      owner: "",
+      newNotes: [],
+      inProgress: [],
+      complete: []
+    };
+  },
+  methods: {
+    addSticky() {
+      this.newNotes.push({ task: this.task, owner: this.owner });
+      (this.task = ""), (this.owner = "");
+    },
+    removeSticky(index, note) {
+      if (this.newNotes.includes(note)) {
+        this.newNotes.splice(index, 1);
+      }
+      if (this.inProgress.includes(note)) {
+        this.inProgress.splice(index, 1);
+      }
+      if (this.complete.includes(note)) {
+        this.complete.splice(index, 1);
+      }
+    },
+    moveSticky(index, note) {
+      if (this.newNotes.includes(note)) {
+        this.inProgress.push(note);
+        this.newNotes.splice(index, 1);
+        return;
+      }
+      if (this.inProgress.includes(note)) {
+        this.complete.push(note);
+        this.inProgress.splice(index, 1);
+        return;
+      }
+    },
+    assignOwner(noteProps) {
+      console.log(noteProps);
+      var allNotes = [this.newNotes, this.inProgress, this.complete];
+      allNotes.forEach(stickyGroup => {
+        stickyGroup.forEach(element => {
+          if (element.task == noteProps[1]) {
+            element.owner = noteProps[0];
+          }
+        });
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#noteInput {
+  width: 75%;
+}
+#board {
+  min-height: 100vh;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+#addBtn {
+  margin: 15px;
+}
+#newNotes,
+#inProgress {
+  border-right: 1px solid black;
+}
+h2{
+  padding: 20px;
 }
 </style>
---
+++ src/components/sticky-note.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="note" @dblclick="menu = !menu">
+    <p>{{task}}</p>
+    <p id="owner" v-if="owner">{{owner}}</p>
+    <div id="menu" v-if="menu">
+        <div v-if="!owner">
+        <button @click="assign">Sign</button>
+        <input v-model="newOwn" type="text" placeholder="assign to">
+      </div>
+      <b-button @click="deleteNote" size="sm">Delete sticky</b-button>
+      <b-button @click="moveNote" size="md">></b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: String,
+    owner: String
+  },
+  data() {
+    return {
+      menu: false,
+      assigned: false,
+      newOwn: ''
+    };
+  },
+  methods: {
+      deleteNote(){
+          this.$emit('deleteNote')
+      },
+      moveNote(){
+          this.$emit('moveNote')
+      },
+      assign(){
+          this.$emit('assign', [this.newOwn, this.task])
+      }
+  }
+};
+</script>
+
+<style>
+#note {
+  background: lightgoldenrodyellow;
+  max-width: 250px;
+  max-height: 250px;
+  min-height: 50px;
+  margin: 15px;
+
+  box-shadow: 4px 4px 2px grey;
+  text-align: left;
+}
+p {
+  padding: 10px;
+}
+#menu {
+  padding: 10px;
+}
+#owner {
+  font-size: 12px;
+}
+#message {
+  padding: 10px;
+}
+</style>
