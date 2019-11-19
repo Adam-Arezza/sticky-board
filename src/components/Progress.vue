2fd2296eb0b62e39c816bc2ff7454dfeb1f0e6a4
Adam-Arezza <arezza.adam@gmail.com>
2019-11-19 09:49:57 -0500

    Added progress bar to show % of completed tasks

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="progress">{{percentComplete}}</div>
+  <div class="progress">
+    <strong>{{percentComplete}} completed</strong>
+    <div class="progress-bar" ref="p-bar">
+      <div class="increment" v-for="(percent, index) in percent" :key="index">|</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["stickies", "columns"],
+  data() {
+    return {
+      percent: 0
+    }
+  },
   computed: {
     percentComplete() {
       const completed = [];
@@ -16,14 +26,17 @@ export default {
       let completeTasks = completed.length;
       let total = this.stickies.length;
       if (completeTasks < total && completed[0] != undefined) {
-        console.log("some amount is completed");
+        //console.log("some amount is completed");
         const percentage = (completeTasks / total).toPrecision(2);
+        this.percent = percentage * 100
         return percentage * 100 + "%";
       }
       if (completeTasks == total && completed[0] != undefined) {
         const percentage = (completeTasks / total).toPrecision(2);
+        this.percent = percentage * 100
         return percentage * 100 + "%";
       } else {
+        this.percent = 0
         return 0 + "%";
       }
     }
@@ -32,4 +45,15 @@ export default {
 </script>
 
 <style scoped>
+.progress-bar {
+  display: grid;
+  grid-template-columns: repeat(100, 1fr);
+  align-self: center;
+  background: lightgray;
+  border-radius: 10px;
+  margin: 5px;
+}
+.increment {
+  background: lightgreen;
+}
 </style>
